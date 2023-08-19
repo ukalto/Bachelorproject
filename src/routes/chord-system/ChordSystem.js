1cb21e51042a0bdfe5c1a83f2404bdcd0457bbd1
ukalto <maxi.gaber@gmail.com>
2023-08-20 00:18:02 +0200

    added structure for every subpage

--- src/routes/chord-system/ChordSystem.js
+++ src/routes/chord-system/ChordSystem.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import styled from "styled-components";
 
 const ChordSystem = () => {
-  return <h2>ChordSystem</h2>;
+    return (
+        <FieldGrid>
+          <Field>Field 1</Field>
+          <Field>Field 2</Field>
+          <Field>Field 3</Field>
+          <Field>Field 4</Field>
+        </FieldGrid>
+    );
 };
 
 export default ChordSystem;
+
+const FieldGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 50px;
+`;
+
+const Field = styled.button`
+  width: 100%;
+  padding: 20px;
+  font-size: 100px;
+  background-color: var(---primary); /* Make sure to define these variables in your CSS */
+  border-radius: 10px;
+  border: solid 2px var(---secondary);
+  color: var(---tertiary);
+  box-shadow: -10px -10px 15px -3px rgba(46, 41, 51, 0.08),
+    10px -10px 15px -3px rgba(46, 41, 51, 0.08),
+  -10px 10px 15px -3px rgba(46, 41, 51, 0.08),
+  10px 10px 15px -3px rgba(46, 41, 51, 0.08),
+  0 10px 15px -3px rgba(46, 41, 51, 0.08),
+  0 4px 6px -2px rgba(71, 63, 79, 0.16);
+  cursor: pointer;
+`;
